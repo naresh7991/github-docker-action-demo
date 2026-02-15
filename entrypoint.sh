877b2f7e9d45de062241a396958d4f79bfbe35d2
Naresh <naresh.gopal1997@gmail.com>
2026-02-15 14:18:19 +0530

    fix the input message printing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "Custom Docker Action says: $message"
+echo "Custom Docker Action says: $INPUT_MESSAGE"
 
 time=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 echo "time=$time" >> "$GITHUB_OUTPUT"
